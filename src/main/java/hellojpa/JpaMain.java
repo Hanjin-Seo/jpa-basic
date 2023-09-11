00a82c186c315b41aa5a559b14d6a804973a86a1
unknown <seo970713@gmail.com>
2023-09-12 00:10:38 +0900

    영속성 컨텍스트1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,12 +1,56 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         // META-INF/persistence.xml의 persistence-unit의 name="hello"
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            /*
+                1. INSERT
+             */
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("HelloA");
+//            em.persist(member);
+
+            /*
+                2. SELECT
+             */
+
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println(findMember.getId());
+//            System.out.println(findMember.getName());
+
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(8)
+//                    .getResultList();
+
+            /*
+                3. UPDATE
+             */
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
     }
 }

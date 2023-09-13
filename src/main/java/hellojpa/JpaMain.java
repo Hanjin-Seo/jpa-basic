4fe2ee8096dcc14fd800c949af0d2588f196994d
unknown <seo970713@gmail.com>
2023-09-14 00:25:55 +0900

    영속성 컨텍스트2 -트랜잭션을 지원하는 쓰기 지연

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -44,6 +44,24 @@ public class JpaMain {
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("HelloJPA");
 
+            /*
+                비영속
+             */
+//            Member member = new Member();
+//            member.setId(100L);
+//            member.setName("HelloJPA");
+
+            /*
+                영속
+             */
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("=========================");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
